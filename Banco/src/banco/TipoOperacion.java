83e4556d9c0484e0442c19734781f925fa535264
Carolina Bautista <alphacosmo@hotmail.com>
2012-12-05 21:07:10 -0400

    Eliminar comentarios

--- Banco/src/banco/TipoOperacion.java
+++ Banco/src/banco/TipoOperacion.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package banco;
 
-/**
- *
- * @author Carolina Bautista
- */
+
 public class TipoOperacion {
     
 }
