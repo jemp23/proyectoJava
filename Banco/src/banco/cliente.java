8188517381c904a4361aac6b632f87e89e191a29
shonata canchica <shonata.geobel@gmail.com>
2012-12-06 04:44:36 -0430

    clase cliente

---
+++ Banco/src/banco/cliente.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author usuario
+ */
+public class cliente {
+    
+}
