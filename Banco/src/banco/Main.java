97ae9f48829bc8da888bf87935625010d6b44314
Juan Motta <jemp23@gmail.com>
2012-12-07 02:35:54 -0430

    CAMBIOS EN EL MAIN
    Se cambio la variable que recibe le resultado del menu de int a string,
    para evitar errores al momento de ingresar letras.

--- Banco/src/banco/Main.java
+++ Banco/src/banco/Main.java
@@ -1,6 +1,7 @@
 
 package banco;
 
+import herramienta.Herramienta;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,21 +10,20 @@ import java.io.InputStreamReader;
 public class Main 
 {
 
-    private static void limpiarPantalla(int lineas)
-        {
-            for (int i = 0; i < lineas; i++) 
-                {
-                    System.out.println("");
-                }
-        } 
 
-  public static void main(String[] args) throws IOException {
-        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-        int seleccion;
-        int codigo;
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    
+
+       InputStreamReader entrada = new InputStreamReader(System.in);
+       BufferedReader leer = new BufferedReader(entrada);
+       String entradaTeclado;
+       String respuesta="0";
+       
         
         do {
-            limpiarPantalla(20);
+            
+            Herramienta.limpiarPantalla(20);
             System.out.println("MENU");
             System.out.println("1. Nuevo Cliente");
             System.out.println("2. Tipos de Cuentas");
@@ -31,45 +31,54 @@ public class Main
             System.out.println("4. Nueva Cuenta Bancaria");
             System.out.println("5. Reportes");
             System.out.println("6. Operaciones Bancarias");
-            System.out.println("7. Salir");
+            System.out.println("7. Autores");
+            System.out.println("8. Salir");
             System.out.println("Seleccione una opción.");
 
             
-            seleccion = Integer.parseInt(leer.readLine());
-            switch (seleccion) {
-                case 1:
-                    limpiarPantalla(20);
+            entradaTeclado = leer.readLine();
+            respuesta = entradaTeclado.toString();
+            
+            switch (respuesta) {
+                case "1":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Nuevo Cliente");
                     break;
-                case 2:
-                    limpiarPantalla(20);
+                case "2":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Tipos de Cuentas");
                     break;
-                case 3:
-                    limpiarPantalla(20);
+                case "3":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Tipos de Operaciones");
                     break;
-                case 4:
-                    limpiarPantalla(20);
+                case "4":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Nueva Cuenta Bancaria");
                     break;
-                case 5:
-                    limpiarPantalla(20);
+                case "5":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Operaciones Bancarias");
                     break;
-                case 6:
-                    limpiarPantalla(20);
+                case "6":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Reportes");
                     break;
-                case 7:
-                    limpiarPantalla(20);
+                case "7":
+                    Herramienta.limpiarPantalla(20);
+                    System.out.println("Autores");
+                    break;                    
+                case "8":
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Adios");
                     break;
                 default:
+                    Herramienta.limpiarPantalla(20);
                     System.out.println("Opcion Invalida");
-                    break; // This break is not really necessary
+                    Herramienta.pausa(3000);
+                    
             }
-        } while (seleccion != 7);
+        }while(!"8".equals(respuesta));
     }
 
 }
--- Banco/src/banco/TipoCuenta.java
+++ Banco/src/banco/TipoCuenta.java
@@ -6,12 +6,17 @@ public class TipoCuenta
 private String descripcion="";
 private long idTipocuenta=0;
 
+
 public TipoCuenta()
     {
     }
 public void addTipoCuenta(String descripcion)
     {
-        idTipocuenta++;
+        ++idTipocuenta;
         this.descripcion= descripcion;
+    }
+public void imprimir()
+    {
+        System.out.println("id "+idTipocuenta+" Des: "+ descripcion);
     }
 }
