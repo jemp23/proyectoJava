d0b502ad4c3215dae29dc4e20d63987ec4d47731
Shonata Canchica <shonata.geobel@gmail.com>
2012-12-09 09:49:38 -0430

    comentarios en el main

--- Banco/src/banco/Main.java
+++ Banco/src/banco/Main.java
@@ -15,6 +15,7 @@ public class Main {
     static int contadorClientes = 0;
     static int contadorOperaciones = 0;
 
+    //metodo para llamar al menu principal
     public static void mostrarMenu() {
         Herramienta.limpiarPantalla(20);
         System.out.println("MENU");
@@ -29,6 +30,7 @@ public class Main {
         System.out.println("Seleccione una opción.");
     }
 
+    //metodo para mostrar el menu de reportes
     public static int mostrarMenuReporte() throws IOException {
         int respuesta = 0;
 
@@ -42,39 +44,49 @@ public class Main {
         System.out.println("6. Listado de Tipo de Cuentas");
         System.out.println("7. Volver");
         System.out.println("Seleccione una opción.");
-
+        
+        //leo una respuesta para enviarsela el metodo que ejecuta la accion
         respuesta = Integer.parseInt(Herramienta.leerEntrada());
 
         return respuesta;
     }
 
+    //este metodo ejecuta la opcion que se seleccione en el mostrarMenuReporte
     public static void seleccioneReporte() throws InterruptedException, IOException {
         int respuesta = 0;
         do {
+            //llamo al menu de los reportes y guardo la seleccion en la variable
             respuesta = mostrarMenuReporte();
             switch (respuesta) {
                 case 1:
+                    //buscar un cliente por el id
                     buscar_cliente();
                     break;
                 case 2:
+                        //buscar una cuenta por su id
                         buscarCuenta();
                     break;
                 case 3:
+                    //imprime un listado de los clientes
                     imprimir_lista_clientes();
                     break;
                 case 4:
 
                     break;
                 case 5:
+                    //imprime un listado con los tipos de operaciones
                     imprimir_tipos_operaciones();
                     break;
                 case 6:
+                    //imprime un listado con los tipos de cuentas
                     imprimirTipodeCuenta();
                     break;
                 case 7:
+                    //vuelve al menu principal
                     System.out.println("Menu Principal");
                     break;
                 default:
+                    //valida si el usurio selecciono una opcion inclorrecta
                     Herramienta.limpiarPantalla(20);
                     System.out.println("Opcion Invalida");
                     Herramienta.pausa(3000);
@@ -82,6 +94,7 @@ public class Main {
         } while (respuesta != 7);;
     }
 
+       //metodo para imprimir las opereaciones a efectuar ene l banco
         public static int mostrarTipoOperaciones() throws IOException 
             {
             int respuesta = 0;
@@ -94,11 +107,12 @@ public class Main {
             respuesta = Integer.parseInt(Herramienta.leerEntrada());
             return respuesta;
             }
- 
+        //metodo que ejecuta la seleccion de operaciones de retiro o abono
      public static void seleccioneOperacion() throws InterruptedException, IOException {
         int respuesta = 0;
         do {
             Herramienta.limpiarPantalla(20);
+            //llamo el menu operaciones y devuelvo el valor a lavariable
             respuesta = mostrarTipoOperaciones();
             switch (respuesta) {
                 case 1:
@@ -114,14 +128,17 @@ public class Main {
                     Herramienta.pausa(3000);
             }
         } while (respuesta != 3);;
-    }       
+    }
+     //metodo para crear un nuevo tipo de cuenta 
     public static void nuevoTipodeCuenta() throws IOException, InterruptedException {
 
         char resp = 's';
         try {
-                
+                //hacer miestras el usuario conteste que si
             do {
                 Herramienta.limpiarPantalla(20);
+                
+                //verifico que no llegue al tope del vector
                 if (contadorTipoCuenta < 10) 
                     {
                     tipoCuenta[contadorTipoCuenta] = new TipoCuenta();
@@ -143,6 +160,7 @@ public class Main {
             }
     }
 
+    //metodo para imprimir el tipo los tipos de cuanta
     public static void imprimirTipodeCuenta() throws InterruptedException {
         Herramienta.limpiarPantalla(20);
         System.out.println("IMPRIMIR TIPOS DE CUENTA");
@@ -153,11 +171,12 @@ public class Main {
         Herramienta.pausa(3000);
     }
 
+    //metodo para registrar un nuevo cliente
     public static void nuevo_cliente() {
         char resp = 's';
         
         try {
-                
+                //hacer miestra el usuario contesta que si
             do {
                 Herramienta.limpiarPantalla(20);
                 System.out.println("Ingrese el Nombre del Cliente");
@@ -173,14 +192,17 @@ public class Main {
         }
     }
 
+    //metodo para buscar un cliente por su id
     public static void buscar_cliente() {
         char resp;
         Herramienta.limpiarPantalla(20);
         try {            
             do {
                 int pos = -1;
+                //leo la entrada del cliente a buscar
                 System.out.println("Ingrese el Id del Cliente");
                 int id = Integer.parseInt(Herramienta.leerEntrada());
+                //valido que exista el cliente
                 for (int i = 0; i < contadorClientes; i++) {
                     if (clientes[i].getIdCliente() == id) {
                         pos = i;
@@ -189,14 +211,14 @@ public class Main {
                 }
                 if(pos == -1){
                     System.out.println(" No hay coincidencias.");
-                }else{
+                }else{//si existe los muestro
                     System.out.println(" ID  \tNOMBRE");
                     System.out.println(clientes[pos].getIdCliente()+" \t"+clientes[pos].getNombreCliente());
                     System.out.println("Cuentas:");
                     for (int j = 0; j < clientes[pos].getContadorCuentas(); j++) {
                             System.out.println(clientes[pos].cuentas[j].getTipoCuenta()+": "+clientes[pos].cuentas[j].getNumeroCuenta());                    
                     }
-                }
+                }//pregunto y leo si el usuario quiere vustar otro cliente
                 System.out.println("Desea Buscar Otro Cliente? s/n");
                 resp = Herramienta.leerEntrada().charAt(0);
             } while (resp == 's' || resp == 'S');
@@ -207,7 +229,7 @@ public class Main {
     }
 
    
-    
+    //metodo para imprimir un listado de los clientes
     public static void imprimir_lista_clientes() {
         Herramienta.limpiarPantalla(20);
         try {
@@ -215,7 +237,7 @@ public class Main {
             for (int i = 0; i < contadorClientes; i++) {
                 System.out.println(clientes[i].getIdCliente() + "  \t " + clientes[i].getNombreCliente());
             }
-            
+            //espero que el usaurio presione una tecla para continuar
             Herramienta.presioneUnaTecla();
             
         } catch (IOException ex) {
@@ -224,12 +246,15 @@ public class Main {
 
     }
 
+    //metodo para abrir una cuanta bancaria
     public static void abrir_cuenta_bancaria(){
         char resp;
         try {            
+            //hacer miestra el usuario diga que si
             do {
                 Herramienta.limpiarPantalla(20);
                 int pos = -1;
+                //consulto si existe el cliente
                 System.out.println("Ingrese el Id del Cliente");
                 int id = Integer.parseInt(Herramienta.leerEntrada());
                 for (int i = 0; i < contadorClientes; i++) {
@@ -242,16 +267,19 @@ public class Main {
                     System.out.println(" No hay coincidencias.");
                 }else{
                     System.out.println("Nombre: "+clientes[pos].getNombreCliente());
-                   
+                   //solicito el numero de cuenta
                     System.out.println("Ingrese el Nro de Cuenta:");
                     String cuenta = Herramienta.leerEntrada();
                     
+                    //busco un listado de tipo de cuenta
                     for (int i = 0; i < contadorTipoCuenta; i++) {
                         System.out.println((i+1)+". \t"+tipoCuenta[i].getDescripcion());
                         
                     }
+                    //seleccione el tipo de cunta por el id
                     System.out.println("Seleccione el tipo de Cuenta:");
                     int tipo_cuenta = Integer.parseInt(Herramienta.leerEntrada());
+                    //registro la cuenta del usuario
                     clientes[pos].setCuentas(cuenta,tipoCuenta[tipo_cuenta-1].getDescripcion());
                 }
                 
@@ -263,7 +291,7 @@ public class Main {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }  
     }
-    
+    //registro los tipo de operaciones
     public static void ingresar_tipos_operaciones(){
         char resp = 's';
         try {
@@ -282,6 +310,7 @@ public class Main {
         }
     }  
     
+    //imprimir los tipos de operaciones
     public static void imprimir_tipos_operaciones() throws InterruptedException {
         System.out.println("TIPOS DE OPERACIONES");
         for (int i = 0; i < contadorOperaciones; i++) {
@@ -289,7 +318,8 @@ public class Main {
         }
         Herramienta.pausa(3000);
     }
-    
+
+//metodo para buscar una cuenta    
 public static void buscarCuenta() throws IOException 
         {
 char resp = 's';
@@ -335,9 +365,10 @@ char resp = 's';
         }
  }
        
-
+    //metodo main
     public static void main(String[] args) throws IOException, InterruptedException {
         int respuesta;
+        //para registrar los tipo de cuenta
         tipoCuenta = new TipoCuenta[10];
 
 
@@ -349,31 +380,40 @@ char resp = 's';
 
             switch (respuesta) {
                 case 1:
-                      nuevo_cliente();
+                    //crear nuevo cliente  
+                    nuevo_cliente();
                     break;
                 case 2:
+                    //Crear nuevo tipo de cuenta
                     nuevoTipodeCuenta();
                     break;
                 case 3:
+                      //crear tipo de operaciones  
                       ingresar_tipos_operaciones();
                       break;
                 case 4:
+                      //abrir nueva cuenta bancaria  
                       abrir_cuenta_bancaria();
                       break;
                 case 5:
+                      //mostrar el menu de reportes  
                       seleccioneReporte();
                       break;
                 case 6:
+                        //mostrar el menu de operaciones
                       seleccioneOperacion();
                       break;
                 case 7:
+                        //nombre de los autores del programa
                        Herramienta.acercaDe();
                        break;
                 case 8:
+                    //salir del sistema
                     Herramienta.limpiarPantalla(20);
                     System.out.println("Adios");
                     break;
                 default:
+                    //cuando la opcion no es correcta
                     Herramienta.limpiarPantalla(20);
                     System.out.println("Opcion Invalida");
                     Herramienta.pausa(3000);
